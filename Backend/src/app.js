cbc718e3f01c526309d02c06152967d72e84aae3
Adarsh1212-21 <adarshshukla1212@gmail.com>
2026-05-03 16:22:52 +0530

    update API

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,8 +7,11 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
+  origin: [
+    "http://localhost:5173",
+    "https://prep-wise-ai-nu.vercel.app"  // ← add this
+  ],
+  credentials: true
 }))
 
 /* require all the routes here */
