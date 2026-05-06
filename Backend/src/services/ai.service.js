d11a53d8b19574282c98e21843d3b768bbc9e483
Adarsh1212-21 <adarshshukla1212@gmail.com>
2026-05-06 23:28:38 +0530

    fix

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -43,7 +43,7 @@ async function generateInterviewReport({ resume, selfDescription, jobDescription
 `
 
     const response = await ai.models.generateContent({
-        model: "gemini-1.5-flash",
+        model: "gemini-2.0-flash",
         contents: prompt,
         config: {
             responseMimeType: "application/json",
@@ -78,7 +78,7 @@ async function generateResumePdf({ resume, selfDescription, jobDescription }) {
                     `
 
     const response = await ai.models.generateContent({
-        model: "gemini-1.5-flash",
+        model: "gemini-2.0-flash",
         contents: prompt,
         config: {
             responseMimeType: "application/json",
